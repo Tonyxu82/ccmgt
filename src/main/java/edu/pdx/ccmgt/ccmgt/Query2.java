d4c5ae2814d00ed958964ed639b7a21f73344377
Tony Xu <vincexu369@gmail.com>
2016-03-15 15:30:32 -0700

    Update on Query2

--- src/main/java/edu/pdx/ccmgt/ccmgt/Query2.java
+++ src/main/java/edu/pdx/ccmgt/ccmgt/Query2.java
@@ -10,26 +10,18 @@ public class Query2 extends Query{
 		//super.connect("localhost",9042,"freeway");
 		System.out.println("Query 2 To Do Here");
 		result1=session.execute("select detectorid from detector_data where locationtext='Foster NB'");
-		StringBuilder tempquery=new StringBuilder();
-		tempquery.append("Select sum(volume) as sum_volume from loop where detectorid IN (");
-
-		int i=0;
+		
+		int volume=0;
 		for (Row row : result1) {
-			if(i>0){
-				tempquery.append(",");
-				i+=1;
+			String query2="Select sum(volume) as sum_volume from loop where detectorid=";
+			query2+=row.getInt("detectorid");
+			query2+=" and starttime>='2011-09-15 00:00:00+0000' and starttime<'2011-09-16 00:00:00+0000'";
+			result2=session.execute(query2);
+			for(Row row1:result2){
+				volume+=row1.getInt("sum_volume");
 			}
-			
-			tempquery.append(row.getInt("detectorid"));
-			System.out.println(row.getInt("detectorid"));
-			i+=1;
 		}
-		tempquery.append(") and starttime>='2011-09-15 00:00:00+0000' and starttime<'2011-09-16 00:00:00+0000'");
-		String query2 = tempquery.toString();
-		System.out.println(query2);
-		result2=session.execute(query2);
-		for(Row row:result2){
-			System.out.format("%d\n", row.getInt("sum_volume"));
-		}
+		System.out.format("%d\n",volume);
+
 	}
 };
